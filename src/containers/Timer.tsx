3294eb3a1edd13a893809e089a2c87295fe91fce
Muhammad Afuzarahman <theafuza@gmail.com>
2024-03-18 14:47:11 -0700

    CreateTimer container

---
+++ src/containers/Timer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Button } from '@/components/Button';
+
+export const Timer = () => {
+  return (
+    <div className="flex flex-col align-center bg-primary-light rounded-lg py-5">
+      <div className="flex flex-row justify-center align-center mb-5">
+        <Button small containerClass="mx-1">
+          POMODORO
+        </Button>
+        <Button small containerClass="mx-1">
+          SHORT BREAK
+        </Button>
+        <Button small containerClass="mx-1">
+          LONG BREAK
+        </Button>
+      </div>
+      <h1 className="flex justify-center align-center mb-5 text-5xl text-primary font-extrabold">
+        30:00
+      </h1>
+      <div className="flex justify-center align-center">
+        <Button>START</Button>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/containers/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#4B2E83',
-        'secondary': '#8347A8',
+        primary: '#4B2E83',
+        secondary: '#8347A8',
         'primary-light': '#F3E3FF',
-
+      },
+      fontSize: {
+        '2xs': '0.6rem',
       },
     },
   },
