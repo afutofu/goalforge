252580c2f02c95baba2bd486ecb735583585f179
Muhammad Afuzarahman <theafuza@gmail.com>
2024-03-18 14:47:27 -0700

    Integrate components in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,58 @@
-export default function Home() {
+import React, { type FC } from 'react';
+
+import { Header } from '@/components/Header';
+import { Timer } from '@/containers/Timer';
+import { Separator } from '@/components/Separator';
+
+interface SectionProps {
+  children: JSX.Element[] | JSX.Element;
+  className?: string;
+}
+
+const Section: FC<SectionProps> = ({ children, className }) => {
+  return (
+    <section
+      className={
+        'w-1/3 border-2 min-h-screen flex flex-col justify-start align-center py-5 px-20 z-20 ' +
+        className
+      }
+    >
+      {children}
+    </section>
+  );
+};
+
+const Home = () => {
   return (
     <main className="flex min-h-screen max-w-full flex-row items-center justify-evenly bg-[url('/images/purplepatternbackground.png')] bg-cover text-white">
       <div className="absolute flex min-h-screen w-full flex-row items-center justify-evenly bg-primary opacity-50 z-10"></div>
-        <section className="w-1/3 border-2 min-h-screen flex flex-col p-5 z-20">
-          <div>Hour Logger</div>
-        </section>
-        <section className="w-1/3 border-2 min-h-screen flex flex-col justify-evenly align-center p-5 z-20">
-          <div>Timer</div>
-          <div>Graph</div>
-          <div>Daily Task Todo</div>
-        </section>
-        <section className="w-1/3 border-2 min-h-screen flex flex-col justify-evenly align-center p-5 z-20">
-          <div>Year Goals Todo</div>
-          <div>Month Goals Todo</div>
-          <div>Week Goals Todo</div>
-        </section>
+      <Section>
+        <Header title={'Hour Logger'} />
+        <Separator />
+      </Section>
+      <Section className="!px-12">
+        <Header title={'GoalForge'} />
+        <Timer />
+        <Separator />
+
+        <Header title={'Graph'} />
+        <Separator />
+
+        <Header title={'Daily Tasks'} />
+        <Separator />
+      </Section>
+      <Section>
+        <Header title={'Year Goals'} />
+        <Separator />
+
+        <Header title={'Month Goals'} />
+        <Separator />
+
+        <Header title={'Week Goals'} />
+        <Separator />
+      </Section>
     </main>
   );
-}
+};
+
+export default Home;
