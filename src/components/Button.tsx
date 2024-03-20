cad4663a4bd4df6b0cff05fe8fe0e246ab7522d7
Muhammad Afuzarahman <theafuza@gmail.com>
2024-03-19 20:34:24 -0700

    Add hover state for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import React, { type FC } from 'react';
+import clsx from 'clsx';
 
 interface IButton extends React.ComponentPropsWithoutRef<'button'> {
   children: string;
@@ -14,11 +15,12 @@ export const Button: FC<IButton> = ({
 }) => {
   return (
     <button
-      className={
-        'bg-secondary rounded-full inline uppercase flex justify-center items-center text-white ' +
-        (small != null ? 'py-1 px-3 ' : 'py-2 px-5 ') +
-        (containerClass ?? '')
-      }
+      className={clsx(
+        'bg-secondary rounded-full inline uppercase flex justify-center items-center text-white hover:bg-primary',
+        { 'py-1 px-3': small },
+        { 'py-2 px-5': !(small ?? false) },
+        { containerClass },
+      )}
       {...props}
     >
       <span className={'font-bold ' + (small != null ? 'text-2xs' : 'text-xs')}>
