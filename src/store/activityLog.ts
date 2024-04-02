f15774c859ecc3fa1436df30e12137f5d8c7ad11
Muhammad Afuzarahman <theafuza@gmail.com>
2024-04-01 23:23:23 -0700

    Make activity log store not persist

--- src/store/activityLog.ts
+++ src/store/activityLog.ts
@@ -1,6 +1,6 @@
 import { type IActivityLog } from '@/types';
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+// import { persist } from 'zustand/middleware';
 
 interface IActivityLogStore {
   // Initial activityLog
@@ -22,61 +22,53 @@ interface IActivityLogStore {
   deleteActivityLog: (activityLogID: string) => void;
 }
 
-export const useActivityLogStore = create<IActivityLogStore>()(
-  persist(
-    (set, get) => ({
-      // Initial activityLog
-      activityLogs: [],
+export const useActivityLogStore = create<IActivityLogStore>((set, get) => ({
+  // Initial activityLog
+  activityLogs: [],
 
-      // Set activityLog
-      setActivityLogs: (activityLogs: IActivityLog[]) => {
-        set({ activityLogs });
-      },
+  // Set activityLog
+  setActivityLogs: (activityLogs: IActivityLog[]) => {
+    set({ activityLogs });
+  },
 
-      // Add activityLog
-      addActivityLog: (activityLog: IActivityLog) => {
-        const currentactivityLog = get().activityLogs;
-        set({ activityLogs: [activityLog, ...currentactivityLog] });
+  // Add activityLog
+  addActivityLog: (activityLog: IActivityLog) => {
+    const currentactivityLog = get().activityLogs;
+    set({ activityLogs: [activityLog, ...currentactivityLog] });
 
-        // Make your API call to add the activityLog
-        // api.addTask(activityLog)
-        //   .catch(error => {
-        //     // Handle error and rollback the state update if needed
-        //     set({ ActivityLog: currentactivityLog });
-        //   });
-      },
-      // Edit activityLog
-      editActivityLog: (
-        activityLogID: string,
-        editedActivityLog: IActivityLog,
-      ) => {
-        const currentactivityLog = get().activityLogs;
-        set({
-          activityLogs: currentactivityLog.map((activityLog) => {
-            if (activityLog.ActivityLogID === activityLogID) {
-              return editedActivityLog;
-            }
-            return activityLog;
-          }),
-        });
-      },
+    // Make your API call to add the activityLog
+    // api.addTask(activityLog)
+    //   .catch(error => {
+    //     // Handle error and rollback the state update if needed
+    //     set({ ActivityLog: currentactivityLog });
+    //   });
+  },
+  // Edit activityLog
+  editActivityLog: (activityLogID: string, editedActivityLog: IActivityLog) => {
+    const currentactivityLog = get().activityLogs;
+    set({
+      activityLogs: currentactivityLog.map((activityLog) => {
+        if (activityLog.ActivityLogID === activityLogID) {
+          return editedActivityLog;
+        }
+        return activityLog;
+      }),
+    });
+  },
 
-      // Delete activityLog
-      deleteActivityLog: (activityLogID: string) => {
-        const currentactivityLog = get().activityLogs;
-        set({
-          activityLogs: currentactivityLog.filter(
-            (activityLog) => activityLog.ActivityLogID !== activityLogID,
-          ),
-        });
+  // Delete activityLog
+  deleteActivityLog: (activityLogID: string) => {
+    const currentactivityLog = get().activityLogs;
+    set({
+      activityLogs: currentactivityLog.filter(
+        (activityLog) => activityLog.ActivityLogID !== activityLogID,
+      ),
+    });
 
-        // Make your API call to add the activityLog
-        // api.deleteTask(activityLog).catch((error) => {
-        //   // Handle error and rollback the state update if needed
-        //   set({ ActivityLog: currentactivityLog });
-        // });
-      },
-    }),
-    { name: 'activityLogStore' },
-  ),
-);
+    // Make your API call to add the activityLog
+    // api.deleteTask(activityLog).catch((error) => {
+    //   // Handle error and rollback the state update if needed
+    //   set({ ActivityLog: currentactivityLog });
+    // });
+  },
+}));
